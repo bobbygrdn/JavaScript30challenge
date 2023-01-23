67dfbd05ab1ba064245cccc9e44d80571b1b6c0e
Robert Gordon <bobbygrdn777@gmail.com>
2023-01-22 19:39:28 -0800

    Finishes the logic for the clock

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,4 +1,3 @@
-
 const setDate = () => {
     const secondsHand = document.querySelector('.second-hand');
     const minutesHand = document.querySelector('.min-hand');
@@ -8,11 +7,11 @@ const setDate = () => {
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360 + 90);
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
+    
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360 + 90);
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
-
+    
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360 + 90);
     hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
